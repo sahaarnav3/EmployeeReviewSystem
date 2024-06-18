36d1089a75057d606d49953530872435a37e7997
Arnav Saha <sahaarnav3@gmail.com>
2024-06-18 20:05:30 +0530

    Add Employee Function added in admin view page..

--- controllers/home_page_controller.js
+++ controllers/home_page_controller.js
@@ -1,4 +1,4 @@
-
+const Employee = require('../models/employees');
 
 module.exports.homepage = (req, res) => {
     if(!req.isAuthenticated())
@@ -15,6 +15,24 @@ module.exports.adminPageRender = (req, res) => {
         res.redirect('/login-employee');
     res.render('admin-page.ejs', { backgroundImage: '../images/background2.png', username: req.user.name.toUpperCase() });
 }
+
+module.exports.createEmployeeFromAdmin = async (req, res) => {
+    let newEmployee = "";
+    try {
+        newEmployee = await Employee.create(new Employee({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            role: req.body.role
+        }));
+    } catch (err) {
+        console.log("This error occured in creating the user :- ", err);
+        if (err.code == 11000)
+            return res.json({ 'Error': 'This User Already Exists. Try Again With Another Email' });
+    }
+    return res.redirect('/homepage');
+}
+
 
 module.exports.employeePageRender = (req, res) => {
     res.send("hello bc");
